c3bf2254b314eeb52d98054eb4541b3af8124f20
Maxime Princelle <maxime@princelle.org>
2020-10-29 22:55:52 +0100

    Fix generation attestation

--- generate.js
+++ generate.js
@@ -10,9 +10,9 @@ window.addEventListener("DOMContentLoaded", (event) => {
     document.getElementById("field-firstname").value = urlParams.get("f"); //firstname
     document.getElementById("field-lastname").value = urlParams.get("l"); //lastname
     document.getElementById("field-birthday").value = urlParams.get("b"); //birthday
-    document.getElementById("field-lieunaissance").value = urlParams.get("p"); //place of born
+    document.getElementById("field-placeofbirth").value = urlParams.get("p"); //place of born
     document.getElementById("field-address").value = urlParams.get("a"); //address
-    document.getElementById("field-town").value = urlParams.get("c"); //city
+    document.getElementById("field-city").value = urlParams.get("c"); //city
     document.getElementById("field-zipcode").value = urlParams.get("z"); //zipcode
     const reason = urlParams.get("r"); //reason
     document.getElementById("checkbox-travail").checked = (reason==='travail');
